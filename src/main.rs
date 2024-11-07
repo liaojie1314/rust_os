1817571c028c6d5bef5e28816d434e66962e21da
liaojie1314 <1517438366@qq.com>
2024-11-07 16:31:10 +0800

    global printing interface

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,21 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+//这个函数将在 panic 发生时被调用
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    loop {}
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
+    write!(vga_buffer::WRITER.lock(), ",come number: {} {}", 42, 1.336).unwrap();
+    write!(vga_buffer::WRITER.lock(), "\n").unwrap();
+    println!("https://www.{}.blog", "yuanyuan");
+    loop {
+        panic!("panic test");
+    }
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,7 @@
 use volatile::Volatile;
 use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -119,17 +121,29 @@ impl fmt::Write for Writer {
     }
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
+// 延迟初始化 静态变量在编译时初始化
+lazy_static! {
+    pub static ref  WRITER:Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
-    write!(writer, "veryvarylonglonglonglonglonglonglonglonglonglonglonglonglonglonglong").unwrap();
-    write!(writer, "\n").unwrap();
-    write!(writer, "https://yuanyuan.blog").unwrap();
+    });
+}
+
+// print! 和 println! 宏定义
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
