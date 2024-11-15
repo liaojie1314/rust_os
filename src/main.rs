311c593016701bd3c84842a1396b44883af1b0fe
liaojie1314 <1517438366@qq.com>
2024-11-15 10:52:48 +0800

    implementing page table access

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::println;
 
@@ -21,39 +22,69 @@ fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
 }
 
+entry_point!(kernel_main);
+
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use rust_os::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
+
     println!("https://www.{}.blog", "yuanyuan");
     rust_os::init();
-    // 调用断点异常 insert an int3 to trigger a breakpoint exception
-    // x86_64::instructions::interrupts::int3();
-    // trigger a page fault exception
-    // unsafe {
-    //     *(0xdeadbeef as *mut u8) = 42;
-    // }
-    // this is for "double fault" demonstration
-    // fn stack_overflow() {
-    //     stack_overflow(); // for each recursion, the return address is pushed
-    // }
-    // trigger a stack overflow
-    // stack_overflow();
 
-    // let ptr = 0x20453c as *mut u8;
-    // // read from a code page
-    // unsafe {
-    //     let x = *ptr;
-    // }
-    // println!("read worked");
-    // unsafe {
-    //     *ptr = 42;
-    // }
-    // println!("write worked");
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
-    );
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+        // not mapped eg:0 -> None
+        0,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+        unsafe {
+            let x = *(address as *mut u8);
+            println!("{:?} contains {:}", address, x); // 0 -> panic
+        }
+    }
+    /// 调用断点异常 insert an int3 to trigger a breakpoint exception
+    /// ```rust
+    /// x86_64::instructions::interrupts::int3();
+    /// // trigger a page fault exception
+    /// unsafe {
+    ///     *(0xdeadbeef as *mut u8) = 42;
+    /// }
+    /// ```
+    /// this is for "double fault" demonstration
+    /// ```rust
+    /// fn stack_overflow() {
+    ///     stack_overflow(); // for each recursion, the return address is pushed
+    /// }
+    /// // trigger a stack overflow
+    /// stack_overflow();
+    /// ```
+    ///
+    /// ```rust
+    /// let ptr = 0x20453c as *mut u8;
+    /// // read from a code page
+    /// unsafe {
+    ///     let x = *ptr;
+    /// }
+    /// println!("read worked");
+    /// unsafe {
+    ///     *ptr = 42;
+    /// }
+    /// println!("write worked");
+    /// ```
 
     #[cfg(test)]
     test_main();
